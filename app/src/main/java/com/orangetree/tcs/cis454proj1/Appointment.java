282b4cb3bb3924d09413cd4df943b36eda69e436
Qiwu Zou <zouqiwu@126.com>
2018-01-31 23:51:22 -0500

    Modified Appointment and SystemCalendar

--- app/src/main/java/com/orangetree/tcs/cis454proj1/Appointment.java
+++ app/src/main/java/com/orangetree/tcs/cis454proj1/Appointment.java
@@ -2,23 +2,36 @@ package com.orangetree.tcs.cis454proj1;
 
 import android.provider.CalendarContract;
 
+import com.orangetree.tcs.cis454proj1.acct.AdminAcct;
+import com.orangetree.tcs.cis454proj1.acct.StudentAcct;
+import com.orangetree.tcs.cis454proj1.acct.TutorAcct;
+
 import java.util.Calendar;
 
 /**
  * Created by Qiwu Zou on 2018/1/23.
  */
-// the abstract class of appointment that holds information about the meeting
+
+/**
+ * The abstract class of appointment that holds information about the meeting
+ */
 public class Appointment {
     public int duration;
-    public String student_name, admin_name, tutor_name;
-    public boolean availability;
+    public AdminAcct adminAccount;
+    public StudentAcct studentAccount;
+    public TutorAcct tutorAccount;
+
     public Calendar date = Calendar.getInstance();
-    // constructor
-    Appointment(int year, int month, int day, int hour, int minute, int second, String student, String admin, String tutor, int dur_time){
+    /**
+     *  construct a appointment object given the account information of
+     *  student, admin and tutor, and the start time and duration.
+      */
+
+    Appointment(int year, int month, int day, int hour, int minute, int second, StudentAcct student, AdminAcct admin, TutorAcct tutor, int dur_time){
         date.set(year,month,day,hour,minute,second);
-        student_name = student;
-        admin_name = admin;
-        tutor_name = tutor;
+        studentAccount = student;
+        adminAccount = admin;
+        tutorAccount = tutor;
         duration = dur_time;
 
     }
--- app/src/main/java/com/orangetree/tcs/cis454proj1/SystemCalendar.java
+++ app/src/main/java/com/orangetree/tcs/cis454proj1/SystemCalendar.java
@@ -2,22 +2,62 @@ package com.orangetree.tcs.cis454proj1;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * Created by Qiwu Zou on 2018/1/30.
  */
-// the class has bo be a singleton since we have to maintain there is only one calendar.
+
+/**
+ * The class has bo be a singleton since we have to maintain there is only one calendar.
+ * The singleton is implemented by "getInstance" method.
+ */
 public class SystemCalendar {
     private static SystemCalendar instance;
     private SystemCalendar(){};
+    /**
+     * Use HashMap to map the time stamp to the appointment object.
+     */
     public static HashMap<Calendar, Appointment> appCollection = new HashMap<Calendar, Appointment>();
-    public ArrayList<Appointment> appList = new ArrayList();
+
     public static SystemCalendar getInstance(){
         if (instance == null){
             instance = new SystemCalendar();
         }
 
         return instance;
+    }
+
+    /**
+     * To get the collection of all appointments
+     * @return An array list of appointments
+     */
+    public ArrayList<Appointment> getAppointments (){
+        ArrayList<Appointment> appList = new ArrayList();
+        Iterator it = appCollection.entrySet().iterator();
+        while (it.hasNext()){
+            appList.add((Appointment)it.next());
+        }
+        return appList;
+    }
+
+    public void clearAppoinment (Calendar timeStamp){
+        appCollection.remove(timeStamp);
+    }
+
+    /**
+     * Add an appointment to the Calendar
+     * @param timeStamp
+     * @param appointmentInformation
+     * @return If there was one appointment in that time, then return false.
+     * Otherwise return true
+     */
+    public boolean addAppointment(Calendar timeStamp, Appointment appointmentInformation){
+        if (appCollection.containsKey(timeStamp)){
+            return false;
+        }
+        appCollection.put(timeStamp,appointmentInformation);
+        return true;
     }
 
 }
