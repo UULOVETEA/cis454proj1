891e64e72dedda3f2fdbb9359f89ce01f15d14c9
UULOVETEA <36201460+UULOVETEA@users.noreply.github.com>
2018-02-22 00:38:54 -0500

    adjust the time of splash page

--- app/src/main/java/com/orangetree/tcs/cis454proj1/Activity_Welcome.java
+++ app/src/main/java/com/orangetree/tcs/cis454proj1/Activity_Welcome.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 
 public class Activity_Welcome extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 2000;
     private String name, message;
     private TextView tvWelcome;
 
